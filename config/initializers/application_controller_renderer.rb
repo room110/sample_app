06aa47ca37a81f032b28ba13fa1697b75580c9ac
yukiito <ito.room.room.110@gmail.com>
2020-05-19 17:37:51 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '881c8447b333569828d25cbcd482b5589e2be05c4d5ea682c22950b286f7cc0db89873a1d20933ea41ab879e12082dcc60a9c70165e90aa9ae4037d6cfbf012d'
